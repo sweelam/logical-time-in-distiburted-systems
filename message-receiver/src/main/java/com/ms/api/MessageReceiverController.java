950d3ac48e3d1af1198cd6556f0e83c6b9269c8f
msweelam <Sw33l@m_2021>
2022-04-03 17:09:03 +0400

    adjust the code for vector clock

--- message-receiver/src/main/java/com/ms/api/MessageReceiverController.java
+++ message-receiver/src/main/java/com/ms/api/MessageReceiverController.java
@@ -1,6 +1,7 @@
 package com.ms.api;
 
 import com.ms.time.manager.dto.ScalarPublishedEvent;
+import com.ms.time.manager.dto.VectorPublishedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
-public class MessageReceiverController {
+public class     MessageReceiverController {
     Logger logger = LoggerFactory.getLogger(MessageReceiverController.class);
 
-    @PostMapping("/message")
+    @PostMapping("/scalar-message")
     public String handleMessages(@RequestBody ScalarPublishedEvent event) {
+        logger.info("Received message {} with order {} ", event.message(), event.eventClock().getClock());
+        return "Received message " + event.message();
+    }
+
+
+    @PostMapping("/vector-message")
+    public String handleMessages(@RequestBody VectorPublishedEvent event) {
         logger.info("Received message {} with order {} ", event.message(), event.eventClock().getClock());
         return "Received message " + event.message();
     }
